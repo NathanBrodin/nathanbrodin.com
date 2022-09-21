170407589df7f6809d86e3ca325f45d2daf213ea
Nathan <nathan.brodin07@gmail.com>
2022-09-21 17:00:25 +0300

    Initial commit

--- pages/index.js
+++ pages/index.js
@@ -14,6 +14,7 @@ export default function Home() {
       <main className={styles.main}>
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
+          This is my personal website.
         </h1>
 
         <p className={styles.description}>
