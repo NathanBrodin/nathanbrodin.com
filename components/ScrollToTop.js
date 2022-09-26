93f0a68340ba20ce8d26e1bfa9c583addc2167f6
Nathan <nathan.brodin07@gmail.com>
2022-09-26 15:33:00 +0300

    Scroll apear

--- components/ScrollToTop.js
+++ components/ScrollToTop.js
@@ -1,7 +1,15 @@
 import Image from 'next/image'
+import { useEffect } from 'react';
 import upArrow from '../public/up-arrow.svg';
 
 export default function ScrollToTop() {
+
+    useEffect(() =>{
+        window.addEventListener('scroll', checkScrollTop);
+        return function cleanup() {
+            window.removeEventListener('scroll', checkScrollTop);
+        };
+    }, [])
 
     function handleClick() {
         window.scrollTo({
@@ -10,8 +18,17 @@ export default function ScrollToTop() {
        });
     }
 
+    function checkScrollTop() {
+        if (window.scrollY > 200) {
+            document.getElementById('scroll-to-top').style.display = 'block';
+        } else {
+            document.getElementById('scroll-to-top').style.display = 'none';
+        }
+        
+    }
+
     return (
-        <div className="fixed bottom-0 right-0 mr-4 mb-6">
+        <div className="fixed bottom-0 right-0 mr-4 mb-6" id="scroll-to-top">
             <button type="button" onClick={handleClick} class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm p-2.5 text-center inline-flex items-center mr-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
             <div class="w-5 h-5 -rotate-90">
             <Image src={upArrow} alt="Scroll to top" width={20} height={20} layout="fill"/>
