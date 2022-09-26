aeaac1a73aacd94ff271b0a27265223d562c48f9
Nathan <nathan.brodin07@gmail.com>
2022-09-26 22:23:24 +0300

    Added Smooth scrolling

--- components/Projects.js
+++ components/Projects.js
@@ -1,8 +1,8 @@
 
 export default function Projects() {
   return (
-    <div className="h-screen"  id="projects">
-      <h1 className="max-w-2xl mb-4 text-5xl font-extrabold tracking-tight leading-none md:text-6xl xl:text-7xl text-white">Projects</h1>
-    </div>
+    <section className="h-screen" id="projects">
+      <h1 className="max-w-2xl mb-4 text-4xl font-bold tracking-tight leading-none md:text-4xl xl:text-4xl text-white px-4">Projects</h1>
+    </section>
   )
 }
