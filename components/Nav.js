ef10b6fed6eea61228f01ab603e549bfa158dddc
Nathan <nathan.brodin07@gmail.com>
2022-09-27 14:52:50 +0300

    Navbar UI update

--- components/Nav.js
+++ components/Nav.js
@@ -11,6 +11,10 @@ export default function Nav() {
   const router = useRouter()
   const locale = router.locale
 
+  // TODO: add a function to change the language
+
+  // TODO: Change color of current page in the navbar
+
   function handleLocaleChange(e) {
     new_locale = router.locales[1 - router.locales.indexOf(router.locale)]
     console.log(new_locale)
@@ -19,71 +23,77 @@ export default function Nav() {
   }
 
   return (    
-<nav className="bg-gray-600 bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-10 px-2 sm:px-4 py-2.5 z-50 fixed w-screen">
-  <div className="container flex flex-wrap justify-between items-center mx-auto">
-  <div className="flex items-center">
-    <span className="self-center text-xl font-semibold whitespace-nowrap text-white">Nathan Brodin</span>
-  </div>
-  <div className="flex items-center md:order-2">
-      <button type="button" data-dropdown-toggle="language-dropdown-menu" className="inline-flex justify-center items-center p-2 text-sm text-gray-500 rounded cursor-pointer hover:bg-gray-700 hover:text-white">
-        <div className="mr-2 w-5 h-5 rounded-full bg-orange-600">
-          {/* <Image src={locale === "en" ? en : fr} alt="Change language" width={20} height={20} layout="fill"/> */}
+    <nav className="bg-gray-600 bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-10 px-2 sm:px-4 py-2.5 z-50 fixed w-screen">
+      <div className="container flex flex-wrap justify-between items-center mx-auto">
+        <div className="flex items-center">
+          <span className="self-center text-xl font-semibold whitespace-nowrap text-white">Nathan Brodin</span>
         </div>
-        { locale === "en" ? "English" : "Français" }
-      </button>
-      <div className="navbar hidden z-50 my-4 text-base list-none rounded divide-y divide-gray-100 shadow bg-gray-700" id="language-dropdown-menu" data-popper-reference-hidden="" data-popper-escaped="" data-popper-placement="bottom">
-        <ul className="py-1" role="none">
-          <li>
-            <a href="#" className="block py-2 px-4 text-sm text-gray-400 hover:bg-gray-600 hover:text-white" role="menuitem">
-              <div className="inline-flex items-center">
-                <div className="h-3.5 w-3.5 rounded-full mr-2 bg-primary-600">
-                  {/* <Image src={en} alt="English" width={512} height={512} layout="fill"/> */}
+        <div className="flex items-center md:order-2">
+          <div className="flex items-center md:order-2 ml-12 mr-4">
+             <button type="button" data-dropdown-toggle="language-dropdown-menu" className="inline-flex justify-center items-center p-2 text-sm text-gray-500 rounded cursor-pointer hover:bg-gray-700 hover:text-white">
+                <div className="mr-2 w-5 h-5 rounded-full bg-orange-600">
+                {/* <Image src={locale === "en" ? en : fr} alt="Change language" width={20} height={20} layout="fill"/> */}
                 </div>
-                English
+                { locale === "en" ? "English" : "Français" }
+              </button>
+              <div className="navbar hidden z-50 my-4 text-base list-none rounded divide-y divide-gray-100 shadow bg-gray-700" id="language-dropdown-menu" data-popper-reference-hidden="" data-popper-escaped="" data-popper-placement="bottom">
+                <ul className="py-1" role="none">
+                  <li>
+                    <a href="#" className="block py-2 px-4 text-sm text-gray-400 hover:bg-gray-600 hover:text-white" role="menuitem">
+                      <div className="inline-flex items-center">
+                        <div className="h-3.5 w-3.5 rounded-full mr-2 bg-primary-600">
+                          {/* <Image src={en} alt="English" width={512} height={512} layout="fill"/> */}
+                        </div>
+                        English
+                      </div>
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#" className="block py-2 px-4 text-sm text-gray-400 hover:bg-gray-600 hover:text-white" role="menuitem">
+                      <div className="inline-flex items-center">
+                        <div className="h-3.5 w-3.5 rounded-full mr-2 bg-primary-600">
+                          {/* <Image src={fr} alt="French" width={512} height={512} layout="fill"/> */}
+                        </div>
+                        Français
+                      </div>
+                    </a>
+                  </li>
+                </ul>
               </div>
-            </a>
-          </li>
-          <li>
-            <a href="#" className="block py-2 px-4 text-sm text-gray-400 hover:bg-gray-600 hover:text-white" role="menuitem">
-              <div className="inline-flex items-center">
-                <div className="h-3.5 w-3.5 rounded-full mr-2 bg-primary-600">
-                  {/* <Image src={fr} alt="French" width={512} height={512} layout="fill"/> */}
+              <button data-collapse-toggle="mobile-menu-language-select" type="button" className="inline-flex items-center p-2 ml-1 text-sm  rounded-lg md:hidden focus:outline-none focus:ring-2 text-gray-400 hover:bg-gray-700 focus:ring-gray-600" aria-controls="mobile-menu-language-select" aria-expanded="false">
+                <span className="sr-only">Open main menu</span>
+                <div className="w-6 h-6 bg-primary-300">
+                  {/* <Image src={hamburger} alt="menu" width={20} height={20} layout="fill"/> */}
                 </div>
-                Français
-              </div>
-            </a>
-          </li>
-        </ul>
+              </button>
+            </div>
+            <div className="hidden justify-between items-center w-full md:flex md:w-auto md:order-1" id="mobile-menu-language-select">
+              <ul className="flex flex-col p-4 mt-4  rounded-lg border md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0  border-gray-700">
+                <li>
+                  <Link href="/">
+                    <a className="block py-2 pr-4 pl-3 text-white bg-primary-700 rounded md:bg-transparent md:text-primary-700 md:p-0" aria-current="page">{t("home")}</a>
+                  </Link>
+                </li>
+                <li>
+                  <Link href="#projects" scroll={false} >
+                    <a className="block py-2 pr-4 pl-3  rounded md:p-0 text-gray-400 md:hover:text-white hover:bg-gray-700 hover:text-white md:hover:bg-transparent border-gray-700">{t("projects")}</a> 
+                  </Link>
+                </li>
+                <li>
+                  <Link href="#about" scroll={false} >
+                    <a className="block py-2 pr-4 pl-3  rounded md:p-0 text-gray-400 md:hover:text-white hover:bg-gray-700 hover:text-white md:hover:bg-transparent border-gray-700">{t("about")}</a> 
+                  </Link>
+                </li>
+                <li>
+                  <Link href="#contact" scroll={false} >
+                    <a className="block py-2 pr-4 pl-3  rounded md:p-0 text-gray-400 md:hover:text-white hover:bg-gray-700 hover:text-white md:hover:bg-transparent border-gray-700">{t("contact")}</a>  
+                  </Link>
+                </li>
+              </ul>
+          </div>
+        </div>
       </div>
-      <button data-collapse-toggle="mobile-menu-language-select" type="button" className="inline-flex items-center p-2 ml-1 text-sm  rounded-lg md:hidden focus:outline-none focus:ring-2 text-gray-400 hover:bg-gray-700 focus:ring-gray-600" aria-controls="mobile-menu-language-select" aria-expanded="false">
-      <span className="sr-only">Open main menu</span>
-      <div className="w-6 h-6 bg-primary-300">
-        {/* <Image src={hamburger} alt="menu" width={20} height={20} layout="fill"/> */}
-      </div>
-    </button>
-  </div>
-  <div className="hidden justify-between items-center w-full md:flex md:w-auto md:order-1" id="mobile-menu-language-select">
-    <ul className="flex flex-col p-4 mt-4  rounded-lg border md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0  border-gray-700">
-      <li>
-        <Link href="/">
-          <a className="block py-2 pr-4 pl-3 text-white bg-primary-700 rounded md:bg-transparent md:text-primary-700 md:p-0" aria-current="page">{t("home")}</a>
-        </Link></li>
-      <li>
-        <Link href="#projects" scroll={false} >
-          <a className="block py-2 pr-4 pl-3  rounded md:p-0 text-gray-400 md:hover:text-white hover:bg-gray-700 hover:text-white md:hover:bg-transparent border-gray-700">{t("projects")}</a> 
-        </Link></li>
-      <li>
-        <Link href="#about" scroll={false} >
-          <a className="block py-2 pr-4 pl-3  rounded md:p-0 text-gray-400 md:hover:text-white hover:bg-gray-700 hover:text-white md:hover:bg-transparent border-gray-700">{t("about")}</a> 
-        </Link></li>
-      <li>
-        <Link href="#contact" scroll={false} >
-          <a className="block py-2 pr-4 pl-3  rounded md:p-0 text-gray-400 md:hover:text-white hover:bg-gray-700 hover:text-white md:hover:bg-transparent border-gray-700">{t("contact")}</a>  
-      </Link></li>
-    </ul>
-  </div>
-  </div>
-</nav>
+    </nav>
   )
 
   /* <div className="mr-3 h-6 sm:h-9">
