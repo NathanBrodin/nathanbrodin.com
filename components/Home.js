67faaab69805fe12c4980d547b2c9389ae1c9fd3
Nathan <nathan.brodin07@gmail.com>
2022-09-28 18:13:47 +0300

    Added new transitions

--- components/Home.js
+++ components/Home.js
@@ -24,11 +24,11 @@ export default function Home() {
             <h1 className="max-w-4xl mb-10 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl text-gray-400">
               {t('home_subtitle')}</h1>
             <p className="max-w-2xl mb-6 font-light  lg:mb-8 md:text-lg lg:text-xl text-gray-400">
-              {t("home_subheading", {age: age})} <br></br> {t("home_opportunity", {age: 20})} </p>
+              {t("home_subheading", {age: age})} <Link href={"https://www.esiea.fr"} passHref={true}><a target="_blank" className='text-primary-500 hover:underline'>ESIEA</a></Link>.<br></br> {t("home_opportunity")}</p>
             <Link href="#projects" scroll={false} >
-              <a className="inline-flex items-center justify-center px-5 py-3 mr-3 text-base font-medium text-center text-white rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-900">
+              <a className="group inline-flex items-center justify-center px-5 py-3 mr-3 text-base font-medium text-center text-white rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-900">
                 Get started
-                <Arrow className='w-5 h-5 ml-2 -mr-1'/>
+                <Arrow className='w-5 h-5 ml-2 -mr-1 group-hover:rotate-90 transition-all'/>
               </a>
             </Link>
         </div>            
