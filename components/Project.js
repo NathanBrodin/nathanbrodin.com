5bee26312bd0115870fd1e4ad00e8aa24163fb66
Nathan <nathan.brodin07@gmail.com>
2022-10-02 10:49:23 +0300

    Update projects

--- components/Project.js
+++ components/Project.js
@@ -1,34 +1,34 @@
 import Tag from './Tag'
 import Image from 'next/image'
 import Link from 'next/link'
-import MyPasswordManager from '../public/projects/MyPasswordManager.png'
 import TooltipGitHub from './TooltipGitHub'
 import TooltipLink from './TooltipLink'
 
 export default function Project(props) {
   const project = props.project
+  const tags = project.tags
 
   return (
     <div className='flex flex-col my-4 bg-gray-800 border border-gray-700 rounded-lg md:flex-row'>
       <div className='flex items-center w-3/4 mb-8'>
         <div className='flex flex-col justify-between p-4'>
-          <h5 className='mb-2 text-2xl font-bold tracking-tight text-white'>My Password Manager</h5>
-          <p className='mb-3 font-normal text-gray-400'>A password manager made with React and Firebase</p>
+          <h5 className='mb-2 text-2xl font-bold tracking-tight text-white'>{project.title}</h5>
+          <p className='mb-3 font-normal text-gray-400'>{project.description}</p>
           <div className='flex flex-wrap'>
-            <Tag tagName='React.js'/>
-            <Tag tagName='Firebase'/>
-            <Tag tagName='Javascript'/>   
+            {tags.map((tag) => (
+              <Tag tagName={tag} />
+            ))}
           </div>
           <div className='flex'>
-            <TooltipGitHub githubLink="https://google.com"/>
-            <TooltipLink text='View on Chrome web store' link='https://google.com'/>
+            <TooltipGitHub githubLink={project.github}/>
+            <TooltipLink text={project.linkText} link={project.url}/>
           </div>
         </div>
       </div>
       <div>
-      <Link href={"https://github.com/NathanBrodin/nathanbrodin.com"} passHref={true}>
+      <Link href={project.url} passHref={true}>
         <a target="_blank" className='px-2'>
-          <Image src={MyPasswordManager} alt="MyPasswordManager" className='h-full md:rounded-none md:rounded-r-lg'/>
+          <Image src={project.image} alt={project.title} className='h-full md:rounded-none md:rounded-r-lg' layout='fill'/>
         </a>
       </Link>
       </div>
--- components/sections/Projects.js
+++ components/sections/Projects.js
@@ -4,17 +4,16 @@ import Project from "../Project"
 export default function Projects() {
   const { t } = useTranslation('projects')
   const title = t('project_title')
+  const projectsList = t('projectsList', { count: 1 }, { returnObjects: true })
   
   return (
     <section className="px-24 py-24" id="projects">
       <h1 className="max-w-2xl px-4 mb-4 text-4xl font-bold text-white md:text-4xl xl:text-4xl">{title}</h1>
 
       <div className="flex flex-col justify-center">
-        <Project/>
-        <Project/>
-        <Project/>
-        <Project/>
-        <Project/>
+        {projectsList.map((project) => (
+          <Project project={project} />
+        ))}
       </div>
     </section>
   )
