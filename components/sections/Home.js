7ad2f852cbfa80d49abdf55c8e58932987aaeda4
Nathan <nathan.brodin07@gmail.com>
2022-10-05 19:05:28 +0300

    Added new translation

--- components/sections/Home.js
+++ components/sections/Home.js
@@ -14,7 +14,7 @@ export default function Home() {
   }
 
   return (
-    <section className="relative flex items-center justify-center h-screen" id='home'>
+    <section className="relative flex items-center justify-center h-[110vh]" id='home'>
       <div className="relative z-10 grid max-w-screen-xl px-4 mx-auto lg:gap-8 xl:gap-0 lg:grid-cols-12 ">
         <div className="mr-auto place-self-center lg:col-span-7 ">
             <h1 className="max-w-4xl mb-2 text-sm font-bold text-gray-400 md:text-sm xl:text-lg">
@@ -26,7 +26,7 @@ export default function Home() {
             <p className="max-w-4xl mb-6 font-light text-gray-400 lg:mb-8 md:text-lg lg:text-xl">
               {t("home_subheading", {age: age})} <Link href={"https://www.esiea.fr"} passHref={true}><a target="_blank" className='text-primary-200 hover:underline'>ESIEA</a></Link>.<br></br> {t("home_opportunity")}</p>
             <Link href="#projects" scroll={false} >
-              <a  className="relative inline-flex items-center justify-center p-4 px-5 py-3 overflow-hidden font-medium text-indigo-600 rounded-lg shadow-2xl group">
+              <a  className="relative inline-flex items-center justify-center p-4 px-5 py-3 overflow-hidden font-medium rounded-lg shadow-2xl group">
                 <span className="absolute top-0 left-0 w-40 h-40 -mt-10 -ml-3 transition-all duration-700 rounded-full bg-gradient-start blur-md ease"></span>
                 <span className="absolute inset-0 w-full h-full transition duration-700 group-hover:rotate-180 ease">
                   <span className="absolute bottom-0 left-0 w-24 h-24 -ml-10 rounded-full bg-gradient-middle blur-md"></span>
--- components/sections/Projects.js
+++ components/sections/Projects.js
@@ -8,7 +8,8 @@ export default function Projects() {
   
   return (
     <section className="px-24 py-24" id="projects">
-      <div className="h-1.5 w-24 mb-8 rounded bg-gradient-to-r from-gradient-start to-gradient-end" />
+      <div className="absolute mt-32 rounded-full left-8 h-72 w-72 bg-gradient-end filter blur-xl opacity-60" />
+      <div className="relative h-1.5 w-24 mb-8 rounded bg-gradient-to-r from-gradient-start to-gradient-end" />
       <h1 className="max-w-2xl px-4 mb-4 text-4xl font-bold text-white md:text-4xl xl:text-4xl">{title}</h1>
       <div className="flex flex-col justify-center">
         {projectsList.map((project, index) => (
