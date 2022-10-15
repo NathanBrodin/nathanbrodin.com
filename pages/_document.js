c3f7549105eb3c8bbdd1cb2a915037594a763d13
Nathan <nathan.brodin07@gmail.com>
2022-10-15 14:37:15 +0300

    Added Google Analytics

--- pages/_document.js
+++ pages/_document.js
@@ -11,6 +11,16 @@ export default function Document() {
       <body>
         <Main />
         <NextScript />
+        {/* Global Site Tag (gtag.js) - Google Analytics */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+          `,
+          }}
+          />
       </body>
     </Html>
   )
