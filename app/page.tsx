118d5e1073ff8e97a86b5018559e779b89da183c
Nathan Brodin <nathan.brodin07@gmail.com>
2023-10-04 18:20:55 +0200

    fix: lazy component loading to fix performences

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,26 @@
-import About from "./components/section/about/about";
-import Contact from "./components/section/contact/contact";
+import dynamic from 'next/dynamic'
 import Home from "./components/section/home/home";
-import Projects from "./components/section/projects/projects";
+
+const DynamicProjects = dynamic(() => import("./components/section/projects/projects"), {
+  loading: () => <p>Loading...</p>,
+})
+
+const DynamicAbout = dynamic(() => import("./components/section/about/about"), {
+  loading: () => <p>Loading...</p>,
+})
+
+const DynamicContact = dynamic(() => import("./components/section/contact/contact"), {
+  loading: () => <p>Loading...</p>,
+})
 
 export default function Page() {
   return (
     <main className="relative flex grow flex-col">
       <Home />
       <div className="flex flex-col content-center items-center justify-center px-4 md:px-6">
-        <Projects />
-        <About />
-        <Contact />
+        <DynamicProjects />
+        <DynamicAbout />
+        <DynamicContact />
       </div>
     </main>
   );
