56a58ee65cf20e753a3ad1b3a44aba7f7f61c07c
Nathan Brodin <nathan.brodin07@gmail.com>
2023-10-04 21:10:53 +0200

    testing

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,16 @@
 import dynamic from "next/dynamic";
-import Home from "./components/section/home/home";
+// import Home from "./components/section/home/home";
 
-const DynamicProjects = dynamic(
-  () => import("./components/section/projects/projects"),
-  {
-    loading: () => <p>Loading...</p>,
-  },
-);
+// const DynamicProjects = dynamic(
+//   () => import("./components/section/projects/projects"),
+//   {
+//     loading: () => <p>Loading...</p>,
+//   },
+// );
 
-const DynamicAbout = dynamic(() => import("./components/section/about/about"), {
-  loading: () => <p>Loading...</p>,
-});
+// const DynamicAbout = dynamic(() => import("./components/section/about/about"), {
+//   loading: () => <p>Loading...</p>,
+// });
 
 const DynamicContact = dynamic(
   () => import("./components/section/contact/contact"),
@@ -25,8 +25,8 @@ export default function Page() {
       {/* <Home /> */}
       <div className="flex flex-col content-center items-center justify-center px-4 md:px-6">
         {/* <DynamicProjects />
-        <DynamicAbout />
-        <DynamicContact /> */}
+        <DynamicAbout /> */}
+        <DynamicContact />
       </div>
     </main>
   );
