78af81bab1b665ffd9769302329217e4b03292f4
Nathan Brodin <nathan.brodin07@gmail.com>
2023-10-04 21:39:57 +0200

    Last one

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,12 @@
 import dynamic from "next/dynamic";
 import Home from "./components/section/home/home";
 
-// const DynamicProjects = dynamic(
-//   () => import("./components/section/projects/projects"),
-//   {
-//     loading: () => <p>Loading...</p>,
-//   },
-// );
+const DynamicProjects = dynamic(
+  () => import("./components/section/projects/projects"),
+  {
+    loading: () => <p>Loading...</p>,
+  },
+);
 
 const DynamicAbout = dynamic(() => import("./components/section/about/about"), {
   loading: () => <p>Loading...</p>,
@@ -24,7 +24,7 @@ export default function Page() {
     <main className="relative flex grow flex-col">
       <Home />
       <div className="flex flex-col content-center items-center justify-center px-4 md:px-6">
-        {/* <DynamicProjects /> */}
+        <DynamicProjects />
         <DynamicAbout />
         <DynamicContact />
       </div>
